4188b3b2b63ff09399984fa09efd8d8c8faad1b6
bts-sky <bbq3306@naver.com>
2026-07-31 09:53:18 +0900

    v2.0.1

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.RECRUIT_CONFIG = {
   version: "v2.0.1",
-  googleAppsScriptUrl: ""
+  googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbwkEfFdbSwq49j5jztCNH6Z9N8bob1qDxxC10uLMq90v-sujM-zqkXBe2m88zWB7FHhdw/exec"
 };
