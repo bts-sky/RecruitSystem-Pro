0e6d22623c560c7823e324d02ccbca1942f57905
bts-sky <bbq3306@naver.com>
2026-08-04 11:30:05 +0900

    관리자 Apps Script 연결

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-window.RECRUIT_CONFIG = {
+window.RECRUIT_SYSTEM_CONFIG = {
   version: "v2.0.1",
   googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbwkEfFdbSwq49j5jztCNH6Z9N8bob1qDxxC10uLMq90v-sujM-zqkXBe2m88zWB7FHhdw/exec"
 };
